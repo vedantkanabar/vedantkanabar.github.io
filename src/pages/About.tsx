ecbbefa4268ceb4da3c8c2b154f413528647f4f8
Vedant Kanabar <vedantkanabar@Vedants-MacBook-Air.local>
2025-12-21 21:39:10 -0500

    Deployment fixes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,7 @@
 export default function About() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen text-center">
-            <h1>An about page</h1>
+            <h1>This is an about page</h1>
             <p>Work In Progress …</p>
         </div>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,8 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center min-h-screen text-center">
             <h1>Personal Portfolio</h1>
             <p>Work In Progress …</p>
+            <p>With a tiny test</p>
+            <p>Another one</p>
         </div>
     )
 }
