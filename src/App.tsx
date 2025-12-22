6db494be70dcae1a57bf3512959c75bc63a0633c
Vedant Kanabar <vedantkanabar@Vedants-MacBook-Air.local>
2025-12-22 15:55:59 -0500

    Deployment v0.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import { Navbar } from "./components/Navbar";
 
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
